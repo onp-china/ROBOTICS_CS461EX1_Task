377b58ab2d0171073e97ab1341722e1ba7262014
ZzzGyyyy <2713176759@qq.com>
2026-05-01 17:37:22 +0800

    Fix rollout export gym compatibility

--- mydiffusion_D1/diffusion_policy/gym_util/async_vector_env.py
+++ mydiffusion_D1/diffusion_policy/gym_util/async_vector_env.py
@@ -241,7 +241,7 @@ class AsyncVectorEnv(VectorEnv):
 
         if not self.shared_memory:
             self.observations = concatenate(
-                results, self.observations, self.single_observation_space
+                self.single_observation_space, results, self.observations
             )
 
         return deepcopy(self.observations) if self.copy else self.observations
@@ -304,7 +304,7 @@ class AsyncVectorEnv(VectorEnv):
 
         if not self.shared_memory:
             self.observations = concatenate(
-                observations_list, self.observations, self.single_observation_space
+                self.single_observation_space, observations_list, self.observations
             )
 
         return (
